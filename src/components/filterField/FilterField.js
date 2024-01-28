20a7e961ac2d756104888a8a8ee493aafc3e0336
Teeddy <russudinu15@gmail.com>
2024-01-28 21:55:58 +0200

    filter

--- src/components/filterField/FilterField.js
+++ src/components/filterField/FilterField.js
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 import { Grid, FilterTitle } from "../styles/styles";
 
 import "../styles/styles";
 import { FilterOption } from "./FilterOptions";
-=======
-import "../styles/styles";
->>>>>>> fb49d7f619ea1c1fce116ba6852fb734fc939151
 
 export function FilterField() {
 
 
     
     return (
-<<<<<<< HEAD
         <Grid style={{background: "#2a0033", overflowY: "auto"}} rows="50px" gap="12px"> 
             <FilterTitle>
                 Filter
@@ -22,10 +17,5 @@ export function FilterField() {
             <FilterOption value="Type"/>
             <FilterOption value="ResetFilter"/>
         </Grid>
-=======
-        <div style={{background: "#2a0033"}}>
-        Test
-        </div>
->>>>>>> fb49d7f619ea1c1fce116ba6852fb734fc939151
     )
 }
